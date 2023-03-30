0c96f1be869ee636a63d96fb155dd4a482a81f0b
vishnuxx <vishnuvs2002it@gmail.com>
2023-03-30 15:03:59 +0530

    removed evergreen ui

--- src/App.js
+++ src/App.js
@@ -6,60 +6,21 @@ import UrlPane from "./Components/UrlPane";
 import RequestBar from "./Components/RequestBar";
 import ResponseBar from "./Components/ResponseBar";
 import TabBar from "./Components/TabBar";
-import { mergeTheme, ThemeProvider } from "evergreen-ui";
+
 import ResizePanel from "react-resize-panel";
 
 function App(props) {
-   const theme = mergeTheme("defaultTheme", {
-     components: {
-       Button: {
-         appearances: {
-           default: {
-             color: "white",
-             paddingX: 12,
-             paddingY: 8,
-             borderRadius: "5px",
-             backgroundColor: "#319520",
-             _hover: {
-               backgroundColor: "#3c952d",
-             },
-             _active: {
-               backgroundColor: "#319520",
-             },
-             _focus: {},
-           },
-         },
-       },
-       Input: {
-         appearances: {
-           default: {
-             color: "white",
-             paddingX: 12,
-             paddingY: 8,
-             borderRadius: "5px",
-             backgroundColor: "#319520",
-             _hover: {
-               backgroundColor: "#3c952d",
-             },
-             _active: {
-               backgroundColor: "#319520",
-             },
-             _focus: {},
-           },
-         },
-       },
-     },
-   });
-
+   
   return (
-    <ThemeProvider value={theme}>
-      <div className="App">
-        <AppBar></AppBar>
-        <TabBar></TabBar>
+    <>
+      <UrlPane mediator={props.mediator}></UrlPane>
+      {/* <div className="App">
+        <AppBar></AppBar> 
+       <TabBar></TabBar>
         <div className="main">
-          {/* <MenuBar></MenuBar> */}
+          <MenuBar></MenuBar>
           <Editor>
-            <UrlPane mediator={props.mediator}></UrlPane>
+            
             <div className="viewpage">
               <RequestBar></RequestBar>
               <ResponseBar mediator={props.mediator}></ResponseBar>
@@ -72,8 +33,8 @@ function App(props) {
 
           <HistoryPanel></HistoryPanel>
         </div>
-      </div>
-    </ThemeProvider>
+      </div>  */}
+    </>
   );
 }
 
