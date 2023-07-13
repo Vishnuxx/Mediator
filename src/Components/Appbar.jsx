8f9a78a1c84788f104dc5cbd5b3e93ac672497d3
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-13 23:29:41 +0530

    removed rsuit dependency from appbar

--- src/Components/Appbar.jsx
+++ src/Components/Appbar.jsx
@@ -1,10 +1,10 @@
-import { Stack } from "rsuite";
+
 
 function AppBar() {
   return (
-    <Stack style={{ borderBottom: "1px solid #262626" , padding:"10px" }}>
+    <div style={{ borderBottom: "1px solid #262626" , background: "#262626" , padding:"10px" }}>
       <h5>MEDIATOR</h5>
-    </Stack>
+    </div>
   );
 }
 
