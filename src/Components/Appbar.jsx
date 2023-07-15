012a63117314d15fdc481f4dbb0790680bf03c63
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-16 03:04:31 +0530

    changed appbar color

--- src/Components/Appbar.jsx
+++ src/Components/Appbar.jsx
@@ -1,6 +1,6 @@
 function AppBar() {
   return (
-    <div className="flex flex-row text-gray-400 bg-[#202020] p-[10px] h-[40px]">
+    <div className="flex flex-row text-gray-400 bg-[#171717] p-[10px] h-[40px]">
       <h5 className="text-md">MEDIATOR 2.0</h5>
     </div>
   );
