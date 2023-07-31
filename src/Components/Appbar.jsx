d8f2d23e7d5ff37190c63c9b2fc5223119782be5
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-31 19:45:33 +0530

    Change

--- src/Components/Appbar.jsx
+++ src/Components/Appbar.jsx
@@ -2,7 +2,7 @@ function AppBar() {
   return (
     <div className="flex flex-row text-gray-400 bg-[#171717] border-b border-[#202020] p-[10px] h-fit">
       <h5 className="text-md">
-        MEDIATOR <span className="text-indigo-400">2.0</span>
+        MEDIATOR
       </h5>
     </div>
   );
