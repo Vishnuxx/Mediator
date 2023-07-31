c025fdb12b8a672378395692beb90c324dcb3e1c
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-31 19:24:58 +0530

    Refactor: Appbar

--- src/Components/Appbar.jsx
+++ src/Components/Appbar.jsx
@@ -1,7 +1,9 @@
 function AppBar() {
   return (
-    <div className="flex flex-row text-gray-400 bg-[#171717] p-[10px] h-[40px]">
-      <h5 className="text-md">MEDIATOR 2.0</h5>
+    <div className="flex flex-row text-gray-400 bg-[#171717] border-b border-[#202020] p-[10px] h-fit">
+      <h5 className="text-md">
+        MEDIATOR <span className="text-indigo-400">2.0</span>
+      </h5>
     </div>
   );
 }
