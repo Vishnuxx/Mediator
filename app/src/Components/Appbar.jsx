67c8679364f8c15667928a3d893c967b5ddb3ba6
vishnuxx <vishnuvs2002it@gmail.com>
2023-08-02 19:33:18 +0530

    Add: Logo to appbar

--- app/src/Components/Appbar.jsx
+++ app/src/Components/Appbar.jsx
@@ -1,6 +1,11 @@
+import logo from "../assets/logo.svg"
+
 function AppBar() {
   return (
-    <div className="flex flex-row text-gray-400 bg-[#171717] border-b border-[#202020] p-[10px] h-fit">
+    <div className="flex flex-row items-center text-gray-400 bg-[#171717] border-b border-[#202020] p-[10px] h-fit">
+
+        <img src={logo} className="h-[25px] w-[25px] mr-2" alt="mediator" />
+ 
       <h5 className="text-md">
         MEDIATOR
       </h5>
