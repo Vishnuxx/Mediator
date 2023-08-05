08b3cc688e58627a07533860ee103dda8e31f523
vishnuxx <vishnuvs2002it@gmail.com>
2023-08-05 15:10:09 +0530

    Refactor method names of Params

--- app/src/core/Models/Params.js
+++ app/src/core/Models/Params.js
@@ -1,26 +1,24 @@
 export default function Params() {
 	const params = [{ enabled: true, key: "", value: "" }];
-	this.getParams = () => params;
+	this.get = () => params;
 
-	this.addParam = (key, value) => {
-		params.push({ enabled: true, key: "", value: "" });
+	this.add = () => {
+		params.push({ enabled: true, key: "" , value: "" });
 	};
 
-	this.removeParam = (index) => {
+	this.remove = (index) => {
 		params.splice(index, 1);
 	};
 
-	this.updateParam = (index, key, value) => {
+	this.update = (index, key, value) => {
 		params[index][key] = value;
 	};
 
-	this.parseParams = () => {
+	this.parse = () => {
 		const obj = {};
 		params.map((item, index) => {
-			if (item.enabled) {
-				if ([undefined, null].includes(item.value)) {
-					obj[item.key] = obj[item.key] + "," + item.value;
-				} else {
+			if (item.enabled && ![undefined, null].includes(item.value)) {
+				if (![item.key, item.value].includes("")) {
 					obj[item.key] = item.value;
 				}
 			}
